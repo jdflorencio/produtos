f0fa318b5dde91de5ab9d8a34e95e374560c7040
João Diego <jdflorencio@gmail.com>
2019-09-01 05:46:55 -0300

    criando formulario

--- src/ProdutoNovo.js
+++ src/ProdutoNovo.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 class Produtos extends Component {
+	
 	render() {
-		return <p>
-			Novo produto
-		</p>
+		const { categorias } = this.props
+		return (
+			<div>
+				<h2>Novo Produto</h2>
+				<select ref='categoria'>
+					{categorias.map((c) => <option value={c.id}>{c.categoria} </option> )}
+				</select>
+				<input 
+					placeholder='nome do novo produto'
+					className='form-control'
+					ref='produto' />
+				<button>Salvar</button>			
+			</div>)
 	}
 }
 
