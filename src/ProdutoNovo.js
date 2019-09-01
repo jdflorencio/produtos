4595459d05bfda3b737784478fa2b68b81360db6
João Diego <jdflorencio@gmail.com>
2019-09-01 05:25:52 -0300

    identando

--- src/ProdutoNovo.js
+++ src/ProdutoNovo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 
 class Produtos extends Component {
-    render() {
-       return <p>
-            Novo produto 
-        </p>
-    }
+	render() {
+		return <p>
+			Novo produto
+		</p>
+	}
 }
- export default Produtos
+
+export default Produtos
